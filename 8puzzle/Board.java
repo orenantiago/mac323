61c86ca0d5ef939667c2b318288c6cf779ac4c29
Renan Silva <renan.tssilva@gmail.com>
2018-04-04 20:04:22 -0300

    implement Board methods

--- 8puzzle/Board.java
+++ 8puzzle/Board.java
@@ -25,11 +25,34 @@ public class Board {
     }
     return sb.toString();
   }
-  // public int tileAt(int row, int col)    // tile at (row, col) or 0 if blank
-  // public int size()                      // board size n
-  // public int hamming()                   // number of tiles out of place
+
+  public int tileAt(int row, int col) {
+    return this.tiles[row][col];
+  }
+
+  public int size() {
+    return n;
+  }
+
+  // number of tiles out of place
+  public int hamming() {
+    int position = 1;
+    int outOfPlace = -1;
+
+    for(int row = 0; row < n; row++) {
+      for(int col = 0; col < n; col++) {
+        if(tiles[row][col] != position) {
+          outOfPlace++;
+        }
+        position++;
+      }
+    }
+    return outOfPlace;
+  }
   // public int manhattan()                 // sum of Manhattan distances between tiles and goal
-  // public boolean isGoal()                // is this board the goal board?
+  public boolean isGoal() {
+    return hamming == 0;
+  }
   // public boolean equals(Object y)        // does this board equal y?
   // public Iterable<Board> neighbors()     // all neighboring boards
   // public boolean isSolvable()            // is this board solvable?
@@ -38,5 +61,7 @@ public class Board {
     int [][] tiles = {{0,8,7},{4,5,6},{1,2,3}};
     Board b = new Board(tiles);
     StdOut.println(b);
+    StdOut.println("tile at 0,0 = " + b.tileAt(0, 0));
+    StdOut.println(b.hamming() + " tiles out of position");
   }
 }
