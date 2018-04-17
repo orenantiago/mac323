28dd5bbf5886e9689647065555609a3757c3524f
Renan Silva <renan.tssilva@gmail.com>
2018-04-16 21:47:50 -0300

    fix Board

--- 8puzzle/Board.java
+++ 8puzzle/Board.java
@@ -1,5 +1,3 @@
-import edu.princeton.cs.algs4.Queue;
-import edu.princeton.cs.algs4.StdOut;
 import java.lang.StringBuilder;
 import java.lang.IllegalArgumentException;
 import java.util.Iterator;
@@ -7,20 +5,26 @@ import java.util.Iterator;
 public class Board {
     private int n;
     private int [][] tiles;
+    private int manhattan;
 
-    // create a board from an n-by-n array of tiles,
-    // where tiles[row][col] = tile at (row, col)
     public Board(int[][] tiles) {
+        int total = 0, desiredRow, desiredCol;
         this.n = tiles.length;
         this.tiles = new int [n][n];
         for(int row = 0; row < this.n; row++) {
             for(int col = 0; col < this.n; col++) {
                 this.tiles[row][col] = tiles[row][col];
+                if(this.tiles[row][col] != 0) {
+                    desiredRow = (this.tiles[row][col] - 1) / n;
+                    desiredCol = (this.tiles[row][col] - 1) % n;
+                    total += difference(row, desiredRow);
+                    total += difference(col, desiredCol);
+                }
             }
         }
+        manhattan = total;
     }
 
-    // string representation of this board
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append(n);
@@ -34,19 +38,16 @@ public class Board {
         return sb.toString();
     }
 
-    // tile at (row, col) or 0 if blank
     public int tileAt(int row, int col) {
         if(row < 0 || col < 0 || row >= n || col >= n)
             throw new IllegalArgumentException();
         return this.tiles[row][col];
     }
 
-    // board size n
     public int size() {
         return n;
     }
 
-    // number of tiles out of place
     public int hamming() {
         int position = 1;
         int outOfPlace = -1;
@@ -62,21 +63,8 @@ public class Board {
         return outOfPlace;
     }
 
-    // sum of Manhattan distances between tiles and goal
     public int manhattan() {
-        int total = 0, desiredRow, desiredCol;
-
-        for(int row = 0; row < n; row++) {
-            for(int col = 0; col < n; col++) {
-                if(tiles[row][col] != 0) {
-                    desiredRow = (tiles[row][col] - 1) / n;
-                    desiredCol = (tiles[row][col] - 1) % n;
-                    total += difference(row, desiredRow);
-                    total += difference(col, desiredCol);
-                }
-            }
-        }
-        return total;
+        return manhattan;
     }
 
     private int difference(int x, int y) {
@@ -85,12 +73,10 @@ public class Board {
         return x - y;
     }
 
-    // is this board the goal board?
     public boolean isGoal() {
         return hamming() == 0;
     }
 
-    // does this board equal y?
     public boolean equals(Object y) {
         Board b = (Board) y;
         if(b.size() != n)
@@ -104,7 +90,6 @@ public class Board {
         return true;
     }
 
-    // is this board solvable?
     public boolean isSolvable() {
         return isSolvableForParity(n % 2);
     }
@@ -115,15 +100,15 @@ public class Board {
         int aux;
         for(int row1 = 0; row1 < n; row1++) {
             for(int col1 = 0; col1 < n; col1++) {
+                if(tiles[row1][col1] == 0) {
+                    blankRow = row1;
+                }
                 for(int row2 = row1; row2 < n; row2++) {
                     aux = row2 == row1 ? col1 + 1 : 0;
                     for(int col2 = aux; col2 < n; col2++) {
                         if(tiles[row1][col1] != 0 && tiles[row2][col2] != 0 &&
                                 tiles[row1][col1] > tiles[row2][col2]) {
                             inversions++;
-                        }
-                        if(tiles[row1][col1] == 0) {
-                            blankRow = row1;
                         }
                     }
                 }
@@ -135,7 +120,6 @@ public class Board {
             return inversions % 2 == 0;
     }
 
-    // all neighboring boards
     public Iterable<Board> neighbors() {
         return new NeighborsIterable(this.tiles);
     }
@@ -207,36 +191,5 @@ public class Board {
             this.tiles[emptyRow][emptyCol] = this.tiles[row][col];
             this.tiles[row][col] = aux;
         }
-    }
-
-    // unit testing
-    public static void main(String[] args) {
-        int [][] tiles = {{5,8,7},{4,0,6},{1,2,3}};
-        Board b = new Board(tiles);
-        StdOut.println(b);
-        StdOut.println("tile at 0,0 = " + b.tileAt(0, 0));
-        StdOut.println(b.hamming() + " tiles out of position");
-        StdOut.println("manhattan distance = " + b.manhattan());
-        try {
-            b.tileAt(66,66);
-        } catch(IllegalArgumentException e) {
-            StdOut.println("catch corner case on tileAt function");
-        }
-
-        int[][] tiles2 = {{1,2,3},{4,5,6},{7,8,0}};
-        Board b1 = new Board(tiles2);
-        Board b2 = new Board(tiles2);
-        StdOut.println(b1);
-        StdOut.println(b1.hamming() + " tiles out of position");
-        StdOut.println("manhattan distance = " + b1.manhattan());
-
-        StdOut.println("b should not equal b1 " + !b.equals(b1));
-        StdOut.println("b2 should equal b1 " + b2.equals(b1));
-        StdOut.println(b);
-        for(Board neighbor : b.neighbors()) {
-            StdOut.println(neighbor);
-        }
-        StdOut.println("b should not be solvable " + !b.isSolvable());
-        StdOut.println("b2 should be solvable " + b2.isSolvable());
     }
 }
